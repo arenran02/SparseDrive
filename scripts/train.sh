650903ce5ec2df3292004bf2a7da0c45b372bfbe
arenran02 <arenran02@gmail.com>
2025-09-30 13:09:01 +0900

    chore: modify gpu unit numbers

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,11 @@
 ## stage1
 bash ./tools/dist_train.sh \
    projects/configs/sparsedrive_small_stage1.py \
-   8 \
+   2 \   # you should set the number of gpus according to your own environment
    --deterministic
 
 ## stage2
 bash ./tools/dist_train.sh \
    projects/configs/sparsedrive_small_stage2.py \
-   8 \
+   2 \   # you should set the number of gpus according to your own environment
    --deterministic
